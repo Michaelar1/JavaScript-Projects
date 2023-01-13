71b2eb8649758833cd6c4dcef219dbad17b4981d
Michaela Rehm <112276652+Michaelar1@users.noreply.github.com>
2023-01-13 13:30:02 -0500

    Created index.html and function.js

---
+++ Basic_JavaScript_Projects/Function_Challenge/function.js
@@ -0,0 +1,5 @@
+function myFunction() { // Create myFunction
+    var A = 5;  //Define variable A and assign it an interger value
+    var B = 7; //Define variable B and assign it an interger value
+    document.getElementById("Multiply").innerHTML = A * B; //Executes document.getElementById() method to print A*B
+}
