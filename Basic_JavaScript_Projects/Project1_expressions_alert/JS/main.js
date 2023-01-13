858ce5769df35159678e7edf4d673ba384b61a6e
Michaela Rehm <112276652+Michaelar1@users.noreply.github.com>
2023-01-13 11:36:52 -0500

    Updated main.js
    Added comments

--- Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -1,7 +1,12 @@
-window.alert("Hello, World");
+window.alert("Hello, World"); //Popup that says "Hello, World"
 
-document.write("What\'s up? " + "How are you?");
+document.write("What\'s up? " + "How are you?"); //Concatenates two strings
 
-var Z = " My dog is the cutest!";
+var Z = " My dog is the cutest! "; //Defining a variable and giving it a string value
 
-document.write(Z);
+document.write(Z); //Printing string value of variable Z
+
+var Family = "Addams", Dad = "Gomez", Mom = "Morticia", Daughter = "Wednesday", Son = "Pugsley";
+document.write(Daughter); //Assigning multiple variables in a statement
+
+document.write(7+5); //Printing the result of an expression
