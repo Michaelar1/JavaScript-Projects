0d71259b50c00db0c0edc57bc1e4db410bd0c698
Michaela Rehm <112276652+Michaelar1@users.noreply.github.com>
2023-01-13 12:59:33 -0500

    Created and updated index.html and main.js

---
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function My_First_Function() {
+    var A = "You can dance if you want to, ";
+    A += "You can leave your friends behind..."
+    document.getElementById("Button_Text").innerHTML = A;
+}
