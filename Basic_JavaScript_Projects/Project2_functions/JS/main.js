b43117ef893946ff4561d8365649d36472a34acb
Michaela Rehm <112276652+Michaelar1@users.noreply.github.com>
2023-01-13 13:08:30 -0500

    Updated main.js
    I added comments to each line of main.js.

--- Basic_JavaScript_Projects/Project2_functions/JS/main.js
+++ Basic_JavaScript_Projects/Project2_functions/JS/main.js
@@ -1,5 +1,5 @@
-function My_First_Function() {
-    var A = "You can dance if you want to, ";
-    A += "You can leave your friends behind..."
-    document.getElementById("Button_Text").innerHTML = A;
+function My_First_Function() { //Create function
+    var A = "You can dance if you want to, "; //Define variable A and assign a string value
+    A += "You can leave your friends behind..." //Concatenate A with a second string. 
+    document.getElementById("Button_Text").innerHTML = A; //Print concatenated string in teh element with the "Button_Text" ID
 }
