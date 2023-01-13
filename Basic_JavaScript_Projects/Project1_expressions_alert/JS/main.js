5c815f833916f71fb2c57933df1f37e1f8f917ea
Michaela Rehm <112276652+Michaelar1@users.noreply.github.com>
2023-01-13 11:49:04 -0500

    Updated main.js

--- Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -10,3 +10,9 @@ var Family = "Addams", Dad = "Gomez", Mom = "Morticia", Daughter = "Wednesday", 
 document.write(Daughter); //Assigning multiple variables in a statement
 
 document.write(7+5); //Printing the result of an expression
+
+var Sent1 = " This is the beginning of the string"; //Creating variable Sent1 and assigning it a string value
+
+var Sent2 = " and this is the end of the string."; //Creating variable Sent2 and assigning it a string value
+
+document.write(Sent1+Sent2); //Concatenating Sent1 and Sent2 and printing the resulting string
