58d952f3875414795b55348c0af1dba278a0cc25
Michaela Rehm <112276652+Michaelar1@users.noreply.github.com>
2023-01-14 14:21:21 -0500

    Updated main.js
    Added ==, ===, &&, ||, and ! operators

--- Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -17,3 +17,35 @@ document.write(17 > 7);
 document.write (7 > 17);
 
 console.log(7 + 8);
+
+document.write(7 == 17);
+
+document.write(15 == 15);
+
+X = 7;
+Y = 7;
+document.write(X === Y);
+
+A = "String";
+B = 5;
+document.write(A === B);
+
+W = "nine";
+Z = 9;
+document.write(W === Z);
+
+D = "Jack";
+E = "King";
+document.write(D === E);
+
+document.write(8 < 9 && 7 < 8);
+
+document.write(9 < 8 && 7==4);
+
+document.write(5 > 4 || 4 > 5);
+
+document.write(4 > 5 || 2 > 3);
+
+document.write(!(5 > 6));
+
+document.write(!(6 > 5));
