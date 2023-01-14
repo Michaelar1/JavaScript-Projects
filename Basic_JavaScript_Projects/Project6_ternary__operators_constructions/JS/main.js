86680aeb2910030c53967f28edd031acf07491ab
Michaela Rehm <112276652+Michaelar1@users.noreply.github.com>
2023-01-14 15:54:44 -0500

    Created and updated index.html and main.js

--- Basic_JavaScript_Projects/Project6_ternary__operators_constructions/JS/main.js
+++ Basic_JavaScript_Projects/Project6_ternary__operators_constructions/JS/main.js
@@ -4,3 +4,15 @@ function Ride_Function() { //Using ternary operator to determine whether or not 
     Can_ride = (Height < 52) ? "You are too short":"You are tall enough";
     document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
 }
+
+function Dog(Breed, Age, Color) { //Create an object constructor to establish objects
+    this.Dog_Breed = Breed;
+    this.Dog_Age = Age;
+    this.Dog_Color = Color;
+}
+
+var Jack = new Dog("Border Collie", 5, "Black and White"); //Utilize object constructor and "new" keyword to create objects of the Dog function.
+var Diane = new Dog("Labrador Retriever", 7, "Chocolate");
+function myFunction() {
+    document.getElementById("New_and_This").innerHTML = "Diane has a " + Diane.Dog_Color + " " + Diane.Dog_Breed + " that is " + Diane.Dog_Age + " years old.";
+}
