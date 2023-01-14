d790fd834533922fd3503a665da0aaef59246aab
Michaela Rehm <112276652+Michaelar1@users.noreply.github.com>
2023-01-14 14:04:30 -0500

    Updated main.js, created index.html and main.js

--- Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -3,3 +3,17 @@ document.write(typeof "Music");
 document.write(typeof 739);
 
 document.write("6" + 7);
+
+function infinity() {
+document.getElementById("infinity").innerHTML = 4E350;
+}
+
+function negInfinity() {
+document.getElementById("NegativeInfinity").innerHTML = -4E350;
+}
+
+document.write(17 > 7);
+
+document.write (7 > 17);
+
+console.log(7 + 8);
