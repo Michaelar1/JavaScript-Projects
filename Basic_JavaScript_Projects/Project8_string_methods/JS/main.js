b27e7a4b5298dd946d5f77a51373ccaf27947bd0
Michaela Rehm <112276652+Michaelar1@users.noreply.github.com>
2023-01-16 16:04:10 -0500

    Update index.html and main.js
    Added slice method, upper case method, and search method.

--- Basic_JavaScript_Projects/Project8_string_methods/JS/main.js
+++ Basic_JavaScript_Projects/Project8_string_methods/JS/main.js
@@ -7,3 +7,25 @@ function fullSentences() {      //Concatenate strings to make full sentences
     var whole_sentences = A.concat(B, C, D, E);
     document.getElementById("Concatenate").innerHTML = whole_sentences;
 }
+
+function sliceMethod() {        //Extract a "slice" of the list string and return as its own string.
+    var list = "Pizza, cake, pie, cheese, apple, orange";
+    var section = list.slice(7,11);
+    document.getElementById("Slice").innerHTML = section;
+}
+
+/* 
+Funnily enough, I used the toUpperCase() method in the Scope, Time, and Function project to make sure that the input in line 40 of the HTML would be accepted in the JS code without user error.
+However, I can play with it here, too, for extra practice!
+*/
+
+function Upper() {              //Transform all text in a string to upper case
+    var A = "That rug really tied the room together.";
+    document.getElementById("DudeAbides").innerHTML = A.toUpperCase();
+}
+
+function find() {
+    var S = "I keep a bag of oranges in my kitchen at all times!";
+    var position = S.search("oranges");
+    document.getElementById("Oranges").innerHTML = position;
+}
