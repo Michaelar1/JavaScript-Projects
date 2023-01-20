e5774699e32817c6b0c809a5d811758f1aff924b
Michaela Rehm <112276652+Michaelar1@users.noreply.github.com>
2023-01-20 15:23:55 -0500

    Updated index.html and main.js
    Fixed while loop in main.js and added the Length Property Challenge to both documents. I also added the HTML for the for loop in index.html

--- Basic_JavaScript_Projects/Project10_loops_arrays/JS/main.js
+++ Basic_JavaScript_Projects/Project10_loops_arrays/JS/main.js
@@ -1,9 +1,16 @@
+//While Loop (decrementing)
 function Call_Loop() {
     var Digit = "";
-    var N = 11;
+    var N = 10;
     while (N > 0) {
-        Digit -= "<br>" + N;
+        Digit += "<br>" + N;
         N--;
     }
     document.getElementById("Loop").innerHTML = Digit;
+}
+
+//Length Property
+function lengthProperty() {
+    var text = "My name is Michaela.";
+    document.getElementById("length").innerHTML = text.length;
 }
