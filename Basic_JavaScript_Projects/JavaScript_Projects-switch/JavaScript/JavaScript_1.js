a0bdae952ab4e6213d311a199e44a89616d1fde6
Michaela Rehm <112276652+Michaelar1@users.noreply.github.com>
2023-01-23 17:06:15 -0500

    Created HTML_1.html and JavaScript_!.html
    I created this for the Switch Assignment

---
+++ Basic_JavaScript_Projects/JavaScript_Projects-switch/JavaScript/JavaScript_1.js
@@ -0,0 +1,25 @@
+function menuFunction() {
+    var orderOutput;
+    var Foods = document.getElementById("Food_Input").value;
+    var OrderString = " is an excellent choice! Your order will be ready in 20 minutes."
+    switch(Foods) {
+        case "smorrebrod":
+            orderOutput = "The Sm&#248;rrebr&#248;d Sampler" + OrderString;
+        break;
+        case "frikadeller":
+            orderOutput = "Frikadeller" + OrderString;
+        break;
+        case "chili":
+            orderOutput = "Vegetarian Chili with Cornbread" + OrderString;
+        break;
+        case "mac":
+            orderOutput = "Greens Mac and Cheese" + OrderString;
+        break;
+        case "burger":
+            orderOutput = "The Black Bean Burger" + OrderString;
+        break;
+        default:
+            orderOutput = "Please enter the number of the item on the menu - not the item itself.";            
+    }
+    document.getElementById("output").innerHTML = orderOutput;
+}
